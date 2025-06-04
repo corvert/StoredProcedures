a16a4881278582cd4ef0c2e7a963f98d401acb90
corvert <orvet.priimagi@gmail.com>
2025-06-04 11:02:19 +0300

    model and db connection

---
+++ StoreProc/Data/StoredProcDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreProc.Models;
+
+namespace StoreProc.Data
+{
+    public class StoredProcDbContext : DbContext
+    {
+        public StoredProcDbContext(DbContextOptions<StoredProcDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
