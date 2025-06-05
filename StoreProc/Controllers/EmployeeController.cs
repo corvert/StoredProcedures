060abc09f52ea771f063c35691c8d1f96cd2fd21
corvert <orvet.priimagi@gmail.com>
2025-06-05 12:07:00 +0300

    SearchWithDynamics

--- StoreProc/Controllers/EmployeeController.cs
+++ StoreProc/Controllers/EmployeeController.cs
@@ -114,5 +114,85 @@ namespace StoreProc.Controllers
             }
 
         }
+        [HttpGet]
+        public IActionResult SearchWithDynamics()
+        {
+            string connectionStr = _config.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "dbo.spSearchEmployees";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Employee> employees = new List<Employee>();
+                while (reader.Read())
+                {
+                    var details = new Employee();
+                    details.FirstName = reader["FirstName"].ToString();
+                    details.LastName = reader["LastName"].ToString();
+                    details.Gender = reader["Gender"].ToString();
+                    details.Salary = Convert.ToInt32(reader["Salary"]);
+                    employees.Add(details);
+                }
+                return View(employees);
+            }
+        }
+        [HttpPost]
+        public IActionResult SearchWithDynamics(string firstName, string lastName, string gender, int salary)
+        {
+            string connectionStr = _config.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                StringBuilder sbCommand = new StringBuilder("select * from Employees where 1=1");
+
+                if (firstName != null)
+                {
+                    sbCommand.Append(" and FirstName = @FirstName");
+                    SqlParameter param = new SqlParameter("@FirstName", firstName);
+                    cmd.Parameters.Add(param);
+                }
+                if (lastName != null)
+                {
+                    sbCommand.Append(" and LastName = @LastName");
+                    SqlParameter param = new SqlParameter("@LastName", lastName);
+                    cmd.Parameters.Add(param);
+                }
+                if (gender != null)
+                {
+                    sbCommand.Append(" and Gender = @Gender");
+                    SqlParameter param = new SqlParameter("@Gender", gender);
+                    cmd.Parameters.Add(param);
+                }
+                if (salary != 0)
+                {
+                    sbCommand.Append(" and Salary = @Salary");
+                    SqlParameter param = new SqlParameter("@Salary", salary);
+                    cmd.Parameters.Add(param);
+                }
+                cmd.CommandText = sbCommand.ToString();
+                cmd.CommandType = System.Data.CommandType.Text;
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Employee> employees = new List<Employee>();
+                while (reader.Read())
+                {
+                    var details = new Employee();
+                    details.FirstName = reader["FirstName"].ToString();
+                    details.LastName = reader["LastName"].ToString();
+                    details.Gender = reader["Gender"].ToString();
+                    details.Salary = Convert.ToInt32(reader["Salary"]);
+                    employees.Add(details);
+
+                }
+                return View(employees);
+            }
+
+        }
     }
 }
